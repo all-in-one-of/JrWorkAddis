b4733c6157054a2b4ca269c60c0a92d843344d9e
Addison <addisonbmoyer@gmail.com>
2018-11-29 10:55:29 -0700

    some pycharm work

--- Maya/Python/Rename.py
+++ Maya/Python/Rename.py
@@ -1,192 +1,72 @@
 import maya.cmds as cmds
 
+class RenameUI():
+    def __init__(self):
+        self.windowCreator = "Renaming"
+        self.winCreat()
 
-def Selection(string $p1,string $p2,int $numbPad)
-{
-    //Grab selection and init $object
-    string $sels[] = `ls -sl`;
-    string $object;
-    
-    //Get and print the Array size
-    $arraySize = size($sels);
-    print ("Objects Selected " + $arraySize + "\n");
-    
-    //assign p1 and p2 to strings
-    string $newname = $p1;
-    string $newSuffix = $p2;
-    
-    //this int is going to be the incramenting number for use in Incrementor
-    int $newNumber = 00;
-    string $zero = "0";
-    int $numbPadd = $numbPad;
-   
-           
-    // For each object in the selection do this:
-    for($object in $sels)
-    {
-        print ("this object was " +$object + "\n");
-        
-        // generate number
-        if ($numbPadd > 1 && $newNumber <10)
-        {
-        rename $object($newname + $zero + $newNumber + $newSuffix);
-        print ("this object is now " + $p1 + $newNumber + $p2 + "\n");
-        $newNumber++;
-        }
-        else
-        {
-        $newNumber++;
-        rename $object($newname + $newNumber + $newSuffix);
-        }
-        
-        print ("this object is now " + $p1 + $newNumber + $p2 + "\n");
-    }
-}
+    def winCreat(self):
+        self.delete()
+        #get user parameters via window
+        self.windowCreator = cmds.window(self.windowCreator, title='Renaming Window')
+        self.colLayout = cmds.columnLayout(parent=self.windowCreator, adjustableColumn=True)
+        cmds.text(label='Renamer', parent=self.colLayout)
+        cmds.textFieldGrp('NewName', label='Enter new Name Here', parent=self.colLayout)
+        dropDwnM = cmds.optionMenu('Suffix', parent=self.colLayout, label='Select Suffix')
+        cmds.menuItem(parent=dropDwnM, label=' Jnt')
+        cmds.menuItem(parent=dropDwnM, label=' Geo')
+        cmds.menuItem(parent=dropDwnM, label=' Ctrl')
+        cmds.menuItem(parent=dropDwnM, label=' SNRTHIS')
+        cmds.optionMenu('Padd', label='Select Number Padding')
+        cmds.menuItem(label='None')
+        cmds.menuItem(label='0')
+        cmds.menuItem(label='00')
+        cmds.button(parent=self.colLayout, label="Rename the Thing!!!", command=lambda x: self.Renamer())
 
-global proc Renaming(string $splitThis)
-{
-    //take input and tokenize it.
-	string $name[];
-	tokenize $splitThis "#" $name;
-	
-	//assign strings to each part of array
-	string $part1 = $name[0];
-	string $part2 = $name[1];
-	
-	print ($splitThis + "\n");
-	print ($part1+ "\n");
-	print ($part2+ "\n");
-	
-	//get the sizes
-    $stringSize = size($splitThis);
-    print("The whole string size" + $stringSize);
-    int $noPadChars = size($part1) + size($part2);
-    print("the letter characters" + $noPadChars);
-    int $numberPadding = $stringSize - $noPadChars;
-    print("The Padding is" + $numberPadding);
-    
 
-    
-	
-	//Call Selection Function
-    Selection($part1,$part2,$numberPadding);
+        cmds.showWindow(self.windowCreator)
 
-}
-    //init default string
-        string $stringToSplit;
-    //Check if window with this name exists delete window if so
+        #For each object in list Rename To:
+        # Prefix (user input)  + Number (that counts) + Suffix (user input or drop down)
+    def delete(self):
+        if (cmds.window('Renaming', exists=True)): cmds.deleteUI('Renaming')
+    def Renamer(self):
+        #Get selection into list
+        self.sel = cmds.ls(sl=True)
 
-        if (`window -exists RenameWindow`) deleteUI -window RenameWindow;
+        self.counter = 1
+        for objet in self.sel:
+            self.name = cmds.textFieldGrp('NewName', q=True, tx=True)
+            self.suffix = cmds.optionMenu('Suffix', q=True, v=True)
 
-    //make new window 
-        window RenameWindow;
-        columnLayout;
-    //i'm using uneditable fields to give instruction
-        text -label "Renaming Rules";
-        scrollField -wordWrap true -height 60 -text "This will rename all selected items based off of the name of the first name you enter here" -editable false;
-        scrollField -wordWrap true -height 60 -text "Type the name you want with a '#' in between first half and last half, EXAMPLE BELOW" -editable false;
+            padding = cmds.optionMenu('Padd', q=True, v=True)
 
-    //now this is what the user enters ast the first option. the renamer will calculate the rest.
-        string $userInput = `textFieldGrp -label "Renamer" -text "firstPart_#_LastPart"`;
-        button -label "Print Text" -command ("string $currentUsersInput = `textFieldGrp -q -text $userInput`; print($currentUsersInput)");   
-        print ($stringToSplit);
+            if padding == '00' and self.counter < 10:
+                NewestName = self.name + ' ' + str(0) + str(0) + str(self.counter) + self.suffix
+                cmds.rename(objet, NewestName)
+                self.counter += 1
 
-    //Call Renaming with this button by getting what the user put in. assigning it to the string to split and running rename.
-        button -label "Rename all Selected" -command ("$stringToSplit = `textFieldGrp -q -text $userInput`; string $currentUsersInput = `textFieldGrp -q -text $userInput`; Renaming($stringToSplit)");
-global proc Selection(string $p1,string $p2,int $numbPad)
-{
-    //Grab selection and init $object
-    string $sels[] = `ls -sl`;
-    string $object;
-    
-    //Get and print the Array size
-    $arraySize = size($sels);
-    print ("Objects Selected " + $arraySize + "\n");
-    
-    //assign p1 and p2 to strings
-    string $newname = $p1;
-    string $newSuffix = $p2;
-    
-    //this int is going to be the incramenting number for use in Incrementor
-    int $newNumber = 00;
-    string $zero = "0";
-    int $numbPadd = $numbPad;
-   
-           
-    // For each object in the selection do this:
-    for($object in $sels)
-    {
-        print ("this object was " +$object + "\n");
-        
-        // generate number
-        if ($numbPadd > 1 && $newNumber <10)
-        {
-        rename $object($newname + $zero + $newNumber + $newSuffix);
-        print ("this object is now " + $p1 + $newNumber + $p2 + "\n");
-        $newNumber++;
-        }
-        else
-        {
-        $newNumber++;
-        rename $object($newname + $newNumber + $newSuffix);
-        }
-        
-        print ("this object is now " + $p1 + $newNumber + $p2 + "\n");
-    }
-}
+            elif padding == '00' and 10 <= self.counter < 100:
+                NewestName = self.name + ' ' + str(0) + str(self.counter) + self.suffix
+                cmds.rename(objet, NewestName)
+                self.counter += 1
 
-global proc Renaming(string $splitThis)
-{
-    //take input and tokenize it.
-	string $name[];
-	tokenize $splitThis "#" $name;
-	
-	//assign strings to each part of array
-	string $part1 = $name[0];
-	string $part2 = $name[1];
-	
-	print ($splitThis + "\n");
-	print ($part1+ "\n");
-	print ($part2+ "\n");
-	
-	//get the sizes
-    $stringSize = size($splitThis);
-    print("The whole string size" + $stringSize);
-    int $noPadChars = size($part1) + size($part2);
-    print("the letter characters" + $noPadChars);
-    int $numberPadding = $stringSize - $noPadChars;
-    print("The Padding is" + $numberPadding);
-    
+            elif padding == '0' and self.counter < 10:
+                NewestName = self.name + ' ' + str(0) + str(self.counter) + self.suffix
+                cmds.rename(objet, NewestName)
+                self.counter += 1
 
-    
-	
-	//Call Selection Function
-    Selection($part1,$part2,$numberPadding);
+            elif padding == 'None':
+                NewestName = self.name + ' ' + str(self.counter) + self.suffix
+                cmds.rename(objet, NewestName)
+                self.counter += 1
+            else:
+                cmds.warning("No Valid Padd Number! Doing Default No Pad")
+                NewestName = self.name + ' ' + str(self.counter) + self.suffix
+                cmds.rename(objet, NewestName)
+                self.counter += 1
 
-}
-    //init default string
-        string $stringToSplit;
-    //Check if window with this name exists delete window if so
-        if (`window -exists RenameWindow`) deleteUI -window RenameWindow;
+        print NewestName
+        self.counter = 1
 
-    //make new window 
-        window RenameWindow;
-        columnLayout;
-    //i'm using uneditable fields to give instruction
-        text -label "Renaming Rules";
-        scrollField -wordWrap true -height 60 -text "This will rename all selected items based off of the name of the first name you enter here" -editable false;
-        scrollField -wordWrap true -height 60 -text "Type the name you want with a '#' in between first half and last half, EXAMPLE BELOW" -editable false;
-
-    //now this is what the user enters ast the first option. the renamer will calculate the rest.
-        string $userInput = `textFieldGrp -label "Renamer" -text "firstPart_#_LastPart"`;
-        button -label "Print Text" -command ("string $currentUsersInput = `textFieldGrp -q -text $userInput`; print($currentUsersInput)");   
-       // print ($stringToSplit);
-
-    //Call Renaming with this button by getting what the user put in. assigning it to the string to split and running rename.
-        button -label "Rename all Selected" -command ("$stringToSplit = `textFieldGrp -q -text $userInput`; string $currentUsersInput = `textFieldGrp -q -text $userInput`; Renaming($stringToSplit)");
-global proc RenamingWindow()
-{
-        showWindow RenameWindow;
-}
-
-RenamingWindow();
+RenameUI()
