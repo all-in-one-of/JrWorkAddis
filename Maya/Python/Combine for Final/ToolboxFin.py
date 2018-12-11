55864dd01706069041fe17ee8b36e5cb62a7214a
Addison <addisonbmoyer@gmail.com>
2018-12-11 11:03:32 -0700

    Tool box comming along

--- Maya/Python/Combine for Final/ToolboxFin.py
+++ Maya/Python/Combine for Final/ToolboxFin.py
@@ -1,5 +1,6 @@
 import maya.cmds as cmds
 
+
 class Toolbox():
     def __init__(self):
         self.myWin = 'amToolbox'
@@ -10,12 +11,12 @@ class Toolbox():
 
         self.myWin = cmds.window(self.myWin, title='amToolbox')
         self.myColl = cmds.columnLayout(parent=self.myWin, adjustableColumn=True)
-        cmds.button(parent=self.myColl, command=lambda x: self.DuplicateWind())
-        cmds.button(parent=self.myColl, command=lambda x: self.locatorWind())
-        cmds.button(parent=self.myColl, command=lambda x: self.CtrlWind())
-        cmds.button(parent=self.myColl, command=lambda x: self.ConstraintWind())
-        cmds.button(parent=self.myColl, command=lambda x: self.RenameWind())
-        cmds.button(parent=self.myColl, command=lambda x: self.RandomWind())
+        cmds.button(label="Duplicate Selected", parent=self.myColl, command=lambda x:cmds.duplicate())
+        cmds.button(label="Create Locator Window", parent=self.myColl, command=lambda x: self.locatorWind())
+        cmds.button(label="Control Creator Window", parent=self.myColl, command=lambda x: self.CtrlWind())
+        cmds.button(label="Parent Constraint W/ Options Window", parent=self.myColl, command=lambda x: self.ConstraintWind())
+        cmds.button(label="Renaming Window", parent=self.myColl, command=lambda x: self.RenameWind())
+        cmds.button(label="Randomizer Window", parent=self.myColl, command=lambda x: self.RandomWind())
 
         cmds.showWindow(self.myWin)
 
@@ -30,5 +31,23 @@ class Toolbox():
         LocatorTool = CreateLocator.Locators()
         LocatorTool.windowMaker()
 
+    def CtrlWind(self):
+        import FinalControlCreator
+        ControlCreate = FinalControlCreator.ControlsSir()
+        ControlCreate.ControlCreation()
+
+    def RenameWind(self):
+        import Rename
+        Renamer = Rename.RenameUI()
+        Renamer.winCreat()
+
+    def RandomWind(self):
+        import RandomizerSelf
+        Randomize = RandomizerSelf.randoValuez()
+        Randomize.RandomValues()
+
+    def ConstraintWind(self):
+        print "in Development"
+        cmds.warning("in Development")
 
 Toolbox()
