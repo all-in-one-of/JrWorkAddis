32d954db2c9bfb394a65a75f0598db224ac3ac61
Unknown <addisonbmoyer@gmail.com>
2019-01-28 00:02:54 -0700

    Triggers Sorta Working

---
+++ Level Design/Rainforest Level/Assets/Code/KeyPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+
+
+public class KeyPickUp: MonoBehaviour
+{
+	public GameEvent EnterEvent;
+	public GameEvent ExitEvent;
+	
+	private void OnTriggerEnter(Collider other)
+	{
+	    Debug.Log("Trigger activate!");
+		EnterEvent.Raise();
+	}
+	private void OnTriggerExit(Collider other)
+	{
+	    ExitEvent.Raise();
+	}
+}
+
