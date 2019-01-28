2d51d71dfc48554dc803be6a98ecf89fff4a7a7a
Addison Moyer <addisonbmoyer@gmail.com>
2019-01-28 11:38:42 -0700

    Unity working a bit better

--- Level Design/Rainforest Level/Assets/Code/KeyPickUp.cs
+++ Level Design/Rainforest Level/Assets/Code/KeyPickUp.cs
@@ -7,17 +7,17 @@ using RoboRyanTron.Unite2017.Events;
 
 public class KeyPickUp: MonoBehaviour
 {
+	private bool DoOnce = true;
 	public GameEvent EnterEvent;
-	public GameEvent ExitEvent;
 	
 	private void OnTriggerEnter(Collider other)
 	{
-	    Debug.Log("Trigger activate!");
-		EnterEvent.Raise();
-	}
-	private void OnTriggerExit(Collider other)
-	{
-	    ExitEvent.Raise();
+		if (DoOnce)
+		{
+			Debug.Log("Trigger activate!");
+			EnterEvent.Raise();
+			DoOnce = false;
+		}
 	}
 }
 
